f75b645f6a83b6bc4819f171041a4b1f33a69273
mountakha <mountakha.djigo@ynov.com>
2018-05-15 17:19:39 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,70 @@
+
+<?php
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en | fr">
+<head>
+    <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Document</title>
+    <link rel="stylesheet" href="css1/accueil.css">
+      <link rel="stylesheet" href="css2/">
+      <link rel="icon" href="../../favicon.ico">
+ 
+
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/assets/owl.carousel.min.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/assets/owl.theme.default.min.css'>
+      <link rel="stylesheet" href="css1/product.css">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="index.php">Accueil</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="page2.php">Produit <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="utilisateur.php">user</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="deconnexion.php">Deconnexion</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
